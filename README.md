8f6d92bbcbfca670327b78c277533a085b24850e
Chase Whitener <cwhitener@gmail.com>
2017-07-09 19:24:30 -0400

    Add the various ERROR constants

--- README.md
+++ README.md
@@ -40,7 +40,305 @@ Event loops that work properly on all platforms. YAY!
 
 ## ERROR CONSTANTS
 
+### UV\_E2BIG
+
+Argument list too long
+
+### UV\_EACCES
+
+Permission denied
+
+### UV\_EADDRINUSE
+
+Address already in use
+
+### UV\_EADDRNOTAVAIL
+
+Address not available
+
+### UV\_EAFNOSUPPORT
+
+Address family not supported
+
+### UV\_EAGAIN
+
+Resource temporarily unavailable
+
+### UV\_EAI\_ADDRFAMILY
+
+Address family not supported
+
+### UV\_EAI\_AGAIN
+
+Temporary failure
+
+### UV\_EAI\_BADFLAGS
+
+Bad ai\_flags value
+
+### UV\_EAI\_BADHINTS
+
+Invalid value for hints
+
+### UV\_EAI\_CANCELED
+
+Request canceled
+
+### UV\_EAI\_FAIL
+
+Permanent failure
+
+### UV\_EAI\_FAMILY
+
+ai\_family not supported
+
+### UV\_EAI\_MEMORY
+
+Out of memory
+
+### UV\_EAI\_NODATA
+
+No address
+
+### UV\_EAI\_NONAME
+
+Unknown node or service
+
+### UV\_EAI\_OVERFLOW
+
+Argument buffer overflow
+
+### UV\_EAI\_PROTOCOL
+
+Resolved protocol is unknown
+
+### UV\_EAI\_SERVICE
+
+Service not available for socket type
+
+### UV\_EAI\_SOCKTYPE
+
+Socket type not supported
+
+### UV\_EALREADY
+
+Connection already in progress
+
+### UV\_EBADF
+
+Bad file descriptor
+
 ### UV\_EBUSY
+
+Resource busy or locked
+
+### UV\_ECANCELED
+
+Operation canceled
+
+### UV\_ECHARSET
+
+Invalid Unicode character
+
+### UV\_ECONNABORTED
+
+Software caused connection abort
+
+### UV\_ECONNREFUSED
+
+Connection refused
+
+### UV\_ECONNRESET
+
+Connection reset by peer
+
+### UV\_EDESTADDRREQ
+
+Destination address required
+
+### UV\_EEXIST
+
+File already exists
+
+### UV\_EFAULT
+
+Bad address in system call argument
+
+### UV\_EFBIG
+
+File too large
+
+### UV\_EHOSTUNREACH
+
+Host is unreachable
+
+### UV\_EINTR
+
+Interrupted system call
+
+### UV\_EINVAL
+
+Invalid argument
+
+### UV\_EIO
+
+i/o error
+
+### UV\_EISCONN
+
+Socket is already connected
+
+### UV\_EISDIR
+
+Illegal operation on a directory
+
+### UV\_ELOOP
+
+Too many symbolic links encountered
+
+### UV\_EMFILE
+
+Too many open files
+
+### UV\_EMLINK
+
+Too many links
+
+### UV\_EMSGSIZE
+
+Message too long
+
+### UV\_ENAMETOOLONG
+
+Name too long
+
+### UV\_ENETDOWN
+
+Network is down
+
+### UV\_ENETUNREACH
+
+Network is unreachable
+
+### UV\_ENFILE
+
+File table overflow
+
+### UV\_ENOBUFS
+
+No buffer space available
+
+### UV\_ENODEV
+
+No such device
+
+### UV\_ENOENT
+
+No such file or directory
+
+### UV\_ENOMEM
+
+Not enough memory
+
+### UV\_ENONET
+
+Machine is not on the network
+
+### UV\_ENOPROTOOPT
+
+Protocol not available
+
+### UV\_ENOSPC
+
+No space left on device
+
+### UV\_ENOSYS
+
+Function not implemented
+
+### UV\_ENOTCONN
+
+Socket is not connected
+
+### UV\_ENOTDIR
+
+Not a directory
+
+### UV\_ENOTEMPTY
+
+Directory not empty
+
+### UV\_ENOTSOCK
+
+Socket operation on non-socket
+
+### UV\_ENOTSUP
+
+Operation not supported on socket
+
+### UV\_ENXIO
+
+No such device or address
+
+### UV\_EOF
+
+End of file
+
+### UV\_EPERM
+
+Operation not permitted
+
+### UV\_EPIPE
+
+Broken pipe
+
+### UV\_EPROTO
+
+Protocol error
+
+### UV\_EPROTONOSUPPORT
+
+Protocol not supported
+
+### UV\_EPROTOTYPE
+
+Protocol wrong type for socket
+
+### UV\_ERANGE
+
+Result too large
+
+### UV\_EROFS
+
+Read-only file system
+
+### UV\_ESHUTDOWN
+
+Cannot send after transport endpoint shutdown
+
+### UV\_ESPIPE
+
+Invalid seek
+
+### UV\_ESRCH
+
+No such process
+
+### UV\_ETIMEDOUT
+
+Connection timed out
+
+### UV\_ETXTBSY
+
+Text file is busy
+
+### UV\_EXDEV
+
+Cross-device link not permitted
+
+### UV\_UNKNOWN
+
+Unknown error
 
 ## HANDLE TYPE CONSTANTS
 
@@ -100,6 +398,8 @@ Returns the default loop (which is a singleton object). This module already
 creates the default loop and you get access to it with this method.
 
 ## hrtime
+
+    my $uint64_t = UV::hrtime();
 
 Get the current Hi-Res time (`uint64_t`).
 
--- lib/UV.pm
+++ lib/UV.pm
@@ -84,7 +84,305 @@ Event loops that work properly on all platforms. YAY!
 
 =head2 ERROR CONSTANTS
 
+=head3 UV_E2BIG
+
+Argument list too long
+
+=head3 UV_EACCES
+
+Permission denied
+
+=head3 UV_EADDRINUSE
+
+Address already in use
+
+=head3 UV_EADDRNOTAVAIL
+
+Address not available
+
+=head3 UV_EAFNOSUPPORT
+
+Address family not supported
+
+=head3 UV_EAGAIN
+
+Resource temporarily unavailable
+
+=head3 UV_EAI_ADDRFAMILY
+
+Address family not supported
+
+=head3 UV_EAI_AGAIN
+
+Temporary failure
+
+=head3 UV_EAI_BADFLAGS
+
+Bad ai_flags value
+
+=head3 UV_EAI_BADHINTS
+
+Invalid value for hints
+
+=head3 UV_EAI_CANCELED
+
+Request canceled
+
+=head3 UV_EAI_FAIL
+
+Permanent failure
+
+=head3 UV_EAI_FAMILY
+
+ai_family not supported
+
+=head3 UV_EAI_MEMORY
+
+Out of memory
+
+=head3 UV_EAI_NODATA
+
+No address
+
+=head3 UV_EAI_NONAME
+
+Unknown node or service
+
+=head3 UV_EAI_OVERFLOW
+
+Argument buffer overflow
+
+=head3 UV_EAI_PROTOCOL
+
+Resolved protocol is unknown
+
+=head3 UV_EAI_SERVICE
+
+Service not available for socket type
+
+=head3 UV_EAI_SOCKTYPE
+
+Socket type not supported
+
+=head3 UV_EALREADY
+
+Connection already in progress
+
+=head3 UV_EBADF
+
+Bad file descriptor
+
 =head3 UV_EBUSY
+
+Resource busy or locked
+
+=head3 UV_ECANCELED
+
+Operation canceled
+
+=head3 UV_ECHARSET
+
+Invalid Unicode character
+
+=head3 UV_ECONNABORTED
+
+Software caused connection abort
+
+=head3 UV_ECONNREFUSED
+
+Connection refused
+
+=head3 UV_ECONNRESET
+
+Connection reset by peer
+
+=head3 UV_EDESTADDRREQ
+
+Destination address required
+
+=head3 UV_EEXIST
+
+File already exists
+
+=head3 UV_EFAULT
+
+Bad address in system call argument
+
+=head3 UV_EFBIG
+
+File too large
+
+=head3 UV_EHOSTUNREACH
+
+Host is unreachable
+
+=head3 UV_EINTR
+
+Interrupted system call
+
+=head3 UV_EINVAL
+
+Invalid argument
+
+=head3 UV_EIO
+
+i/o error
+
+=head3 UV_EISCONN
+
+Socket is already connected
+
+=head3 UV_EISDIR
+
+Illegal operation on a directory
+
+=head3 UV_ELOOP
+
+Too many symbolic links encountered
+
+=head3 UV_EMFILE
+
+Too many open files
+
+=head3 UV_EMLINK
+
+Too many links
+
+=head3 UV_EMSGSIZE
+
+Message too long
+
+=head3 UV_ENAMETOOLONG
+
+Name too long
+
+=head3 UV_ENETDOWN
+
+Network is down
+
+=head3 UV_ENETUNREACH
+
+Network is unreachable
+
+=head3 UV_ENFILE
+
+File table overflow
+
+=head3 UV_ENOBUFS
+
+No buffer space available
+
+=head3 UV_ENODEV
+
+No such device
+
+=head3 UV_ENOENT
+
+No such file or directory
+
+=head3 UV_ENOMEM
+
+Not enough memory
+
+=head3 UV_ENONET
+
+Machine is not on the network
+
+=head3 UV_ENOPROTOOPT
+
+Protocol not available
+
+=head3 UV_ENOSPC
+
+No space left on device
+
+=head3 UV_ENOSYS
+
+Function not implemented
+
+=head3 UV_ENOTCONN
+
+Socket is not connected
+
+=head3 UV_ENOTDIR
+
+Not a directory
+
+=head3 UV_ENOTEMPTY
+
+Directory not empty
+
+=head3 UV_ENOTSOCK
+
+Socket operation on non-socket
+
+=head3 UV_ENOTSUP
+
+Operation not supported on socket
+
+=head3 UV_ENXIO
+
+No such device or address
+
+=head3 UV_EOF
+
+End of file
+
+=head3 UV_EPERM
+
+Operation not permitted
+
+=head3 UV_EPIPE
+
+Broken pipe
+
+=head3 UV_EPROTO
+
+Protocol error
+
+=head3 UV_EPROTONOSUPPORT
+
+Protocol not supported
+
+=head3 UV_EPROTOTYPE
+
+Protocol wrong type for socket
+
+=head3 UV_ERANGE
+
+Result too large
+
+=head3 UV_EROFS
+
+Read-only file system
+
+=head3 UV_ESHUTDOWN
+
+Cannot send after transport endpoint shutdown
+
+=head3 UV_ESPIPE
+
+Invalid seek
+
+=head3 UV_ESRCH
+
+No such process
+
+=head3 UV_ETIMEDOUT
+
+Connection timed out
+
+=head3 UV_ETXTBSY
+
+Text file is busy
+
+=head3 UV_EXDEV
+
+Cross-device link not permitted
+
+=head3 UV_UNKNOWN
+
+Unknown error
 
 =head2 HANDLE TYPE CONSTANTS
 
@@ -144,6 +442,8 @@ Returns the default loop (which is a singleton object). This module already
 creates the default loop and you get access to it with this method.
 
 =head2 hrtime
+
+    my $uint64_t = UV::hrtime();
 
 Get the current Hi-Res time (C<uint64_t>).
 
